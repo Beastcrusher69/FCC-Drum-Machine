e868c286dc250f78801c1a9e198c4e31657daa99
Jay <jaykapadia389@gmail.com>
2023-02-16 23:49:46 +0530

    created Drum Machine App using Vite and created basic frame

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  
+  render(){
+    return(
+
+      <div id="drum-machine">
+        {console.log("wixnxnwn")}
+        <div id="keys">
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src="">Q</audio>
+          </button>
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src=""></audio>
+          </button>
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src=""></audio>
+          </button>
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src=""></audio>
+          </button>
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src=""></audio>
+          </button>
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src=""></audio>
+          </button>
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src=""></audio>
+          </button>
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src=""></audio>
+          </button>
+          
+          <button class="drum-pad" id="">
+            <audio class="clip" src=""></audio>
+          </button>
+          
+        </div> 
+        <div id="display">
+          djdjdj xwxwx
+        </div> 
+      </div> 
+    )
+  }
+}
+
+export default App
