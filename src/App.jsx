a301b221f2013f3ed7f8f367beac8ee5cd34284a
Jay <jaykapadia389@gmail.com>
2023-02-17 15:54:11 +0530

    divs are clickable and play sound

--- src/App.jsx
+++ src/App.jsx
@@ -5,52 +5,88 @@ class App extends React.Component{
     super(props);
   }
   
+    playSound = (selector) => {
+    const sound=document.getElementById(selector);
+    sound.play();
+  }
+
   render(){
+
+    const arr = [
+      {
+        keyCode: 81,
+        text: "Q",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+        audioName:"Heater 1"
+      },
+      {
+        keyCode: 87,
+        text: "W",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+        audioName:"Heater 2"
+      },
+      {
+        keyCode: 69,
+        text: "E",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+        audioName:"Heater 3"
+      },
+      {
+        keyCode: 65,
+        text: "A",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+        audioName:"Heater 4"
+      },
+      {
+        keyCode: 83,
+        text: "S",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+        audioName:"Clap"
+      },
+      {
+        keyCode: 68,
+        text: "D",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+        audioName:"Open-HH"
+      },
+      {
+        keyCode: 90,
+        text: "Z",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+        audioName:"Kick-n'-Hat"
+      },
+      {
+        keyCode: 88,
+        text: "X",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+        audioName:"Kick"
+      },
+      {
+        keyCode: 67,
+        text: "C",
+        src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+        audioName:"Closed-HH"
+      }
+    ];
+
     return(
 
       <div id="drum-machine">
-        {console.log("wixnxnwn")}
+        {console.log("jay")}
         <div id="keys">
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src="">Q</audio>
-          </button>
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src=""></audio>
-          </button>
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src=""></audio>
-          </button>
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src=""></audio>
-          </button>
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src=""></audio>
-          </button>
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src=""></audio>
-          </button>
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src=""></audio>
-          </button>
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src=""></audio>
-          </button>
-          
-          <button class="drum-pad" id="">
-            <audio class="clip" src=""></audio>
-          </button>
-          
+          {arr.map((obj) => 
+            <div  onClick={() => { const audio = document.getElementById(obj.text);
+                                    audio.play();}}
+                  className="drum-pad" 
+                  id={obj.audioName}>
+            {obj.text}
+            <audio src={obj.src} 
+                   className="clip" 
+                   id={obj.text}>
+            </audio>
+            </div>)}
         </div> 
         <div id="display">
-          djdjdj xwxwx
         </div> 
       </div> 
     )
